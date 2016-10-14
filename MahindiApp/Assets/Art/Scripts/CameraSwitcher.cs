c8122b5ce8d403891cc61d9b5c9e37781ae24d7b
tylerlybb <tylerlybb@gmail.com>
2016-10-13 21:00:15 -0600

    Added cameras, plant hole and fertilizer hole.

---
+++ MahindiApp/Assets/Art/Scripts/CameraSwitcher.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraSwitcher : MonoBehaviour {
+	public Transform targetfar;
+	public Transform targetmed;
+	public Transform targetclose;
+
+	public void CamFar() {
+		CameraPosition follower = GetComponent<CameraPosition>();
+		if (follower == null) Debug.LogError("Hey, where's the CameraPosition script?!?");
+
+		follower.target = targetfar;
+	}
+	public void CamMed() {
+		CameraPosition follower = GetComponent<CameraPosition>();
+		if (follower == null) Debug.LogError("Hey, where's the CameraPosition script?!?");
+
+		follower.target = targetmed;
+	}
+	public void CamClose() {
+		CameraPosition follower = GetComponent<CameraPosition>();
+		if (follower == null) Debug.LogError("Hey, where's the CameraPosition script?!?");
+
+		follower.target = targetclose;
+	}
+}
---
+++ MahindiApp/Assets/Art/Scripts/Hole.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Hole : MonoBehaviour {
+	Animator anim;
+	int buttonHash1 = Animator.StringToHash("MakeHole");
+	int buttonHash2 = Animator.StringToHash("ResetHole");
+
+	void Start ()
+	{
+		anim = GetComponent<Animator>();
+	}
+
+	public void MakeHole()
+	{
+		anim.SetTrigger (buttonHash1);
+	}
+
+	public void ResetHole()
+	{
+		anim.SetTrigger (buttonHash2);
+	}
+}
