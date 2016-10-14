fed69dec90b73905103331832a01b6e579ff69f9
tylerlybb <tylerlybb@gmail.com>
2016-10-14 07:31:54 -0600

    Got the stick in and planting animation started.

---
+++ MahindiApp/Assets/Art/Scripts/AnimPlant.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AnimPlant : MonoBehaviour {
+	Animator anim;
+	int buttonHash = Animator.StringToHash("Plant");
+
+	void Start ()
+	{
+		anim = GetComponent<Animator>();
+	}
+
+	public void Plant()
+	{
+		anim.SetTrigger (buttonHash);
+	}
+}
