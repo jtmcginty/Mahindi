83e0e335a7fff6f01ca093d019ab0af41b3f38bb
tylerlybb <tylerlybb@gmail.com>
2016-10-15 13:40:52 -0600

    Made leg model with skeleton, converted all animations to Maya, figured out my final pipeline from Maya and organzied files accordingly.

---
+++ MahindiApp/Assets/Art/Scripts/HideShow.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HideShow : MonoBehaviour
+{
+	public Renderer rend;
+
+	void Start ()
+	{
+		rend = GetComponent<Renderer>();
+	}
+
+	public void Show()
+	{
+		rend.enabled = true;
+	}
+	public void Hide()
+	{
+		rend.enabled = false;
+	}
+}
