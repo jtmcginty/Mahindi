85c44a2e488c0da15b9c26ca4b1f70e42da48765
tylerlybb <tylerlybb@gmail.com>
2016-10-16 04:15:15 -0600

    Built and animated hand. Added seeds.

---
+++ MahindiApp/Assets/Art/Scripts/HideShowSkinned.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HideShowSkinned : MonoBehaviour
+{
+	public SkinnedMeshRenderer rend;
+
+	void Start ()
+	{
+		rend = GetComponentInChildren<SkinnedMeshRenderer> ();
+	}
+
+	public void Show()
+	{
+		rend.enabled = true;
+	}
+	public void Hide()
+	{
+		rend.enabled = false;
+	}
+}
