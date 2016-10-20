869377b16a56a314dea89bd412633af6ceb2c41c
tylerlybb <tylerlybb@gmail.com>
2016-10-20 10:29:02 -0600

    First pass of grow animations.

---
+++ MahindiApp/Assets/Art/Scripts/Go.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Go : MonoBehaviour
+{
+	Animator[] anims;
+	int buttonHash = Animator.StringToHash("Go");
+
+	void Start ()
+	{
+		anims = GetComponentsInChildren<Animator>();
+	}
+
+	public void GoNow()
+	{
+		foreach (Animator anim in anims)
+		{			
+			anim.SetTrigger (buttonHash);
+		}
+	}
+}
