87dbed2323ed16afd1a57296ce05fbd4c4e315b2
tylerlybb <tylerlybb@gmail.com>
2016-10-24 02:10:56 -0600

    Got the harvest bag test working with 3 of them.

---
+++ MahindiApp/Assets/Art/Scripts/HideShowChildren.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HideShowChildren : MonoBehaviour {
+	Renderer[] rends;
+
+	void Start () {
+		rends = GetComponentsInChildren<Renderer> ();
+	}
+
+	public void HideChildren() {
+		foreach (Renderer rend in rends) {
+			rend.enabled = false;
+		}
+	}
+
+	public void ShowChildren() {
+		foreach (Renderer rend in rends) {
+			rend.enabled = true;
+		}
+	}
+}
---
+++ MahindiApp/Assets/Art/Scripts/NextBagScript.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NextBagScript : MonoBehaviour {
+	Animator anim;
+	int buttonHash = Animator.StringToHash("NextBag");
+	int buttonHash1 = Animator.StringToHash("NextBag2");
+
+	void Start ()
+	{
+		anim = GetComponent<Animator>();
+	}
+
+	public void NextBag()
+	{
+		anim.SetTrigger (buttonHash);
+	}
+
+	public void NextBag2()
+	{
+		anim.SetTrigger (buttonHash1);
+	}
+}
