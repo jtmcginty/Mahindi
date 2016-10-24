55b953de0b6e375e37541255323d396998efe499
tylerlybb <tylerlybb@gmail.com>
2016-10-24 07:06:48 -0600

    Finished the first harvest test.

---
+++ MahindiApp/Assets/Art/Scripts/Cobs.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Cobs : MonoBehaviour {
+	Animator anim;
+	int buttonHash = Animator.StringToHash("HarvestCobs");
+
+	void Start ()
+	{
+		anim = GetComponent<Animator>();
+	}
+
+	public void HarvestCobs()
+	{
+		anim.SetTrigger (buttonHash);
+	}
+}
