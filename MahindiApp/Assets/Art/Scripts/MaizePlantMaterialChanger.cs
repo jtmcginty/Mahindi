9788014ef72096c27d160dd3f90a74a81d9e55c0
tylerlybb <tylerlybb@gmail.com>
2016-10-26 05:42:30 -0600

    Added color changing to the plants.

---
+++ MahindiApp/Assets/Art/Scripts/MaizePlantMaterialChanger.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MaizePlantMaterialChanger : MonoBehaviour {
+	MaizePlantMaterialMesh[] matTrans;
+
+	void Start () {
+		matTrans = GetComponentsInChildren<MaizePlantMaterialMesh>();
+	}
+
+	public void GreenBrown(float newTarget) {
+		foreach (MaizePlantMaterialMesh matTran in matTrans) {
+			matTran.whichMats = 1;
+			matTran.trackingSpeed = 1;
+			matTran.mixTarget = newTarget;
+		}
+	}
+
+	public void GreenDry(float newTarget) {
+		foreach (MaizePlantMaterialMesh matTran in matTrans) {
+			matTran.whichMats = 2;
+			matTran.trackingSpeed = 1;
+			matTran.mixTarget = newTarget;
+		}
+	}
+
+	public void BrownDry(float newTarget) {
+		foreach (MaizePlantMaterialMesh matTran in matTrans) {
+			matTran.whichMats = 3;
+			matTran.trackingSpeed = 1;
+			matTran.mixTarget = newTarget;
+		}
+	}
+
+	public void SetBrown() {
+		foreach (MaizePlantMaterialMesh matTran in matTrans) {
+			matTran.whichMats = 3;
+			matTran.mix = 0;
+			matTran.mixTarget = 0;
+		}
+	}
+}
---
+++ MahindiApp/Assets/Art/Scripts/MaizePlantMaterialMesh.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MaizePlantMaterialMesh : MonoBehaviour
+{
+	public Material materialGreen;
+	public Material materialBrown;
+	public Material materialDry;
+	[Range(0,1)] public float mix = 0f;
+	[Range(0,1)] public float mixTarget = 0f;
+	public float trackingSpeed = 1f;
+	public int whichMats = 1;
+	Renderer rend;
+
+	void Start()
+	{
+		rend = GetComponent<Renderer> ();
+	}
+	public void Update()
+	{
+		if (mix != mixTarget && trackingSpeed > 0)
+		{
+			mix = Mathf.MoveTowards(mix, mixTarget, trackingSpeed * Time.deltaTime);
+		}
+
+		if (whichMats == 1)
+		{
+			rend.material.Lerp (materialGreen, materialBrown, mix);
+		}
+		if (whichMats == 2)
+		{
+			rend.material.Lerp (materialGreen, materialDry, mix);
+		}
+		if (whichMats == 3)
+		{
+			rend.material.Lerp (materialBrown, materialDry, mix);
+		}
+	}
+
+	public void SetTarget(float newTarget)
+	{
+		mixTarget = newTarget;
+	}
+}
