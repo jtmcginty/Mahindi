747146ddfc3764f1505c3d7755301d49cafe3f4f
tylerlybb <tylerlybb@gmail.com>
2016-10-27 04:21:47 -0600

    Did the first four of the new harvest bag animations.

---
+++ MahindiApp/Assets/Art/Scripts/HarvestBagAnimate.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HarvestBagAnimate : MonoBehaviour
+{
+	Animator[] anims;
+	int buttonHash = Animator.StringToHash("StartHarvest");
+
+	void Start ()
+	{
+		anims = GetComponentsInChildren<Animator>();
+	}
+
+	public void StartHarvest()
+	{
+		foreach (Animator anim in anims)
+		{			
+			anim.SetTrigger (buttonHash);
+		}
+	}
+}
