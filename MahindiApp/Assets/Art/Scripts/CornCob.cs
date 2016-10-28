105397b5093285af793a543d650bace11041b8b4
tylerlybb <tylerlybb@gmail.com>
2016-10-28 02:29:03 -0600

    Finall got the corn cobs animating.

---
+++ MahindiApp/Assets/Art/Scripts/CornCob.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CornCob : MonoBehaviour
+{
+	Animator[] anims;
+	int buttonHash = Animator.StringToHash("PlayCornCob");
+
+	void Start ()
+	{
+		anims = GetComponentsInChildren<Animator>();
+	}
+
+	public void PlayCornCob()
+	{
+		foreach (Animator anim in anims)
+		{			
+			anim.SetTrigger (buttonHash);
+		}
+	}
+}
