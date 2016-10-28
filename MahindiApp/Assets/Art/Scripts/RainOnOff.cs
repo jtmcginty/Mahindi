948db63caa51816a0d2d9573da3fab278643fc16
tylerlybb <tylerlybb@gmail.com>
2016-10-28 15:51:42 -0600

    Made different levels of rain.

--- MahindiApp/Assets/Art/Scripts/RainOnOff.cs
+++ MahindiApp/Assets/Art/Scripts/RainOnOff.cs
@@ -3,21 +3,31 @@ using System.Collections;
 
 public class RainOnOff : MonoBehaviour {
 	Animator anim;
-	int buttonHash1 = Animator.StringToHash("RainOn");
-	int buttonHash2 = Animator.StringToHash("RainOff");
+	int buttonHash1 = Animator.StringToHash("RainHigh");
+	int buttonHash2 = Animator.StringToHash("RainMedium");
+	int buttonHash3 = Animator.StringToHash("RainLow");
+	int buttonHash4 = Animator.StringToHash("RainOff");
 
 	void Start ()
 	{
 		anim = GetComponent<Animator>();
 	}
 
-	public void RainOn()
+	public void RainHigh()
 	{
 		anim.SetTrigger (buttonHash1);
+	}
+	public void RainMedium()
+	{
+		anim.SetTrigger (buttonHash2);
+	}
+	public void RainLow()
+	{
+		anim.SetTrigger (buttonHash3);
 	}
 
 	public void RainOff()
 	{
-		anim.SetTrigger (buttonHash2);
+		anim.SetTrigger (buttonHash4);
 	}
 }
