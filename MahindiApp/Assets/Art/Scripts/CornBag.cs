99e273bf22d4413956363416d0366f8d4d37c00a
tylerlybb <tylerlybb@gmail.com>
2016-10-29 16:13:52 -0600

    Finally figured out the problem with child meshes not animating!!!!! Now I can finally get to finishing the harvest animations! I'm committing this because it has the starter bag in it's working state. Finally.

---
+++ MahindiApp/Assets/Art/Scripts/CornBag.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CornBag : MonoBehaviour
+{
+	Animator[] anims;
+	int buttonHash1 = Animator.StringToHash("Harv1");
+
+	void Start ()
+	{
+		anims = GetComponentsInChildren<Animator>();
+	}
+
+	public void Harv1()
+	{
+		foreach (Animator anim in anims)
+		{			
+			anim.SetTrigger (buttonHash1);
+		}
+	}
+}
