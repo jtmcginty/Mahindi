4aeb86697c499036db1f07a1591a8c2cfb3b5ae3
tylerlybb <tylerlybb@gmail.com>
2016-11-01 21:02:50 -0600

    Added a choreographer object. It is set up to play a "PlantGrowHigh" sequence, which currently is only set up to do two things: zoom the camera in close then trigger the "Plant" animation on the stick and the hand. I wanted to get those few things working before I moved on to complete the rest of the PlantGrowHigh sequence.

---
+++ MahindiApp/Assets/Art/Scripts/Cho_Elements.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Cho_Elements : MonoBehaviour {
+	public CameraSwitcher cameraSwitcherR;
+	public AnimPlant PlantStick;
+	public AnimPlant PlantHand;
+
+	public void CamClose() {
+		cameraSwitcherR.CamClose ();
+	}
+
+	public void Plant() {
+		PlantStick.Plant ();
+		PlantHand.Plant ();
+	}
+}
---
+++ MahindiApp/Assets/Art/Scripts/Cho_Sequences.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Cho_Sequences : MonoBehaviour {
+	Animator anim;
+	int buttonHash = Animator.StringToHash("PlantGrowHigh");
+
+	void Start () {
+		anim = GetComponent<Animator>();
+	}
+
+	public void PlantGrowHigh() {
+		anim.SetTrigger (buttonHash);
+	}
+}
